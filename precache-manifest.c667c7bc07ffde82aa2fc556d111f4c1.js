2438a7715b6f751a39991461ee5583f10f20cd20
ninjapj25 <ninjapj25@gmail.com>
2020-09-20 19:55:15 +0800

    Updates

---
+++ precache-manifest.c667c7bc07ffde82aa2fc556d111f4c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7db31b85b9f7448840831049553fbb0a",
+    "url": "/TypingMaster/index.html"
+  },
+  {
+    "revision": "e6f3eb8f179e9fb894e0",
+    "url": "/TypingMaster/static/css/main.b813fdee.chunk.css"
+  },
+  {
+    "revision": "1c00a296fc230394cf5b",
+    "url": "/TypingMaster/static/js/2.668050b5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TypingMaster/static/js/2.668050b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6f3eb8f179e9fb894e0",
+    "url": "/TypingMaster/static/js/main.ff151b9a.chunk.js"
+  },
+  {
+    "revision": "21d152cc2870cf387a64",
+    "url": "/TypingMaster/static/js/runtime-main.62d98d5d.js"
+  },
+  {
+    "revision": "cf9086951ff5802f51ce268e8a91bf1d",
+    "url": "/TypingMaster/static/media/Logo.cf908695.svg"
+  }
+]);
